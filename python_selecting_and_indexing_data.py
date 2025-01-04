b63798d2749c9e962ad1ec8dd596c488a7fb166b
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-04 15:19:11 -0800

    index and selecting data

--- python_selecting_and_indexing_data.py
+++ python_selecting_and_indexing_data.py
@@ -24,4 +24,12 @@ print("original DataFrame:\n",df)
 # Selecting all rows for a specific column
 print("\nSelecting all rows for column 'A':")
 print(df.loc[:, 'A'])
- 
+
+# Example selecting all rows for multiple columns.
+# import the pandas library and aliasing as pd
+
+df = pd.DataFrame(np.random.randn(8, 4),
+index = ['a','b','c','d','e','f','g','h'], columns = ['A', 'B', 'C', 'D'])
+
+# Select all rows for multiple columns, say list[]
+print(df.loc[:,['A','C']])
