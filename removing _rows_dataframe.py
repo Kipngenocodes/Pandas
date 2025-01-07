cac6e0dc6f08a8d7c2d9599fc5fc55543a6d3a62
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-06 23:16:03 -0800

    Dropping Rows by Labels using the drop() method

--- removing _rows_dataframe.py
+++ removing _rows_dataframe.py
@@ -16,5 +16,22 @@ results  = df.drop(3)
 print('Dropping Rows using the drop() method at index 3:')
 print(results)
 
+
+# Dropping DataFrame Rows by Labels
+# create a dataframe
+df = pd.DataFrame({ 'A': ['A0', 'A1', 'A2', 'A3'],
+                   'B': ['B0', 'B1', 'B2', 'B3'],
+                   'C': ['C0', 'C1', 'C2', 'C3'],
+                   'D': ['D0', 'D1', 'D2', 'D3']
+                   }, index=['r1', 'r2', 'r3', 'r4'])
+
+# Displaying the original DataFrame
+print('Print the original DataFrame:', df)
+# Dropping Rows by Labels using the drop() method
+results = df.drop(['r2', 'r4'])
+print('Dropping Rows by Labels using the drop() method:')
+print(results)
+
+
     
 
