9453e85be95a8f12bc1bb461954db4f257156c7a
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-06 23:44:22 -0800

    Dropping rows by slicing index

--- removing _rows_dataframe.py
+++ removing _rows_dataframe.py
@@ -48,5 +48,21 @@ results = df[(df['A'] != 'A1')]
 print("Removing rows where column 'A' is equal to 'A1':")
 print(results)
 
+# remove rows using slicing of index
+
+# Create a DataFrame
+df = pd.DataFrame({'A': [1, 2, 3, 4, 5],'B': [4, 5, 6, 7, 8]})
+
+# Display original DataFrame
+print("Original DataFrame:")
+print(df)
+
+# Drop the row using the index slicing
+result = df.drop(df.index[2:4])
+
+# Display the result
+print("\nAfter dropping the row at 2 and 3:")
+print(result)
+
 
 
