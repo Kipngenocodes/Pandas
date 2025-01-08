f2d5e28678c732c2e49e7d0d5482a05220fedec7
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-07 21:49:52 -0800

    reading xlsx files using pandas

--- pandas_reading_data_excel.py
+++ pandas_reading_data_excel.py
@@ -4,5 +4,11 @@ Excel files using the pandas.read_excel() method,
 covering different scenarios like loading a single sheet,
 specific sheets, and multiple sheets.'''
 
-data = r'C:\Users\p1\OneDrive\Desktop\Pandas\pandas_reading_data_excel.py'
+data = r'C:\Users\p1\OneDrive\Desktop\Pandas\employee_data.xlsx'
+
+# reading the excel file
+df = pd.read_excel(data)
+
+print("Dataframe:")
+print(df)
 
