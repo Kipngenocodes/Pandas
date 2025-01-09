8ed4f9548fe54b84f925a4f4aa43b5d394c21ed1
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-08 23:18:59 -0800

    reading from clipboard

--- reading_from_clipboard.py
+++ reading_from_clipboard.py
@@ -3,3 +3,7 @@ import pandas as pd
 # Read clipboard content into a DataFrame when using a tabular form
 df = pd.read_clipboard()
 print(df)
+
+# Read clipboard content into a DataFrame when it is non tabular
+df = pd.read_clipboard(sep=',',header=None)
+print(df)
