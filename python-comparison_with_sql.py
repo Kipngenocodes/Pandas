1fc383ffac51bf8ca23b6d5f0daa1b1c4c53db8c
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-10 15:38:55 -0800

    pandas with sql

--- python-comparison_with_sql.py
+++ python-comparison_with_sql.py
@@ -7,8 +7,22 @@ url = 'https://raw.githubusercontent.com/pandas-dev/pandas/main/pandas/tests/io/
 tips=pd.read_csv(url)
 print(tips.head())
 
-
+print('/n')
 # outputing the first 5 rows of the data in pandas
 # Output:
 x = tips[['total_bill', 'tip', 'smoker', 'time']].head(2)
 print(x)
+print('/n')
+
+# filtering rows
+y = tips[tips['time'] == 'Dinner'].head(5)
+print(y)
+print('/n')
+
+p= tips[tips['size'] > 2].head(5)
+print(p)
+
+#print('/n')
+# Grouping data
+g = tips.groupby('sex').size()
+print(g)
