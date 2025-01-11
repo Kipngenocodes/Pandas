180d3e71b4c9184244f48593a69206599c1cd2c1
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-11 15:19:30 -0800

    working with correlation in pandas

---
+++ pandas_correlation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+'''
+Correlation shows the linear relationship between any two array of values (series).
+Pandas corr() function supports different correlation methods,
+including Pearson (default), Spearman, and Kendall.'''
+
+# Create a DataFrame with two columns
+data_frame = pd.DataFrame(np.random.randn(10, 5), columns=['a', 'b', 'c', 'd', 'e'])
+
+print(data_frame['a'].corr(data_frame['b']))
