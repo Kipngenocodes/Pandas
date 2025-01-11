b3d1f670c0dda75cdfa2465bbe4202dd79ed72ff
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-10 23:50:03 -0800

    sorting data using order

--- sorting_by_actualvalues.py
+++ sorting_by_actualvalues.py
@@ -7,3 +7,11 @@ print('unsorted  Pandas series: \n', df)
 
 panda_series_sorted = df.sort_values(ascending=True)
 print("\nSorted Pandas Series: \n", panda_series_sorted)
+
+
+unsorted_df = pd.DataFrame({'col1':[2,9,5,0],'col2':[1,3,2,4]})
+print("Original DataFrame:\n", unsorted_df)
+
+# Sort the DataFrame by values
+sorted_df = unsorted_df.sort_values(by='col1')
+print("\nOutput Sorted DataFrame:\n", sorted_df)
