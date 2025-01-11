97c6114f343c247bc93733e2124993c3de25bc87
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-11 11:01:32 -0800

    final reindexing

--- refilling_while_reindexing_like.py
+++ refilling_while_reindexing_like.py
@@ -14,7 +14,12 @@ df2 = pd.DataFrame(np.random.randn(2, 3), columns=['col1', 'col2', 'col3'])
 
 # # Padding NaNs
 print(df2.reindex_like(df1))
+print('\n')
 
 # Now fill the NaNs with preceding values
 print("Data Frame with Forward Fill:")
 print(df2.reindex_like(df1, method='ffill'))
+print('\n')
+#  Now fill the NaNs with preceding values
+print("Data Frame with Forward Fill limiting to 1:")
+print(df2.reindex_like(df1, method='ffill', limit=1))
