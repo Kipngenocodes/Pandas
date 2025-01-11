b16e7a81c7dc64202732982fd9d72e112599c509
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-10 23:38:36 -0800

    sorting data using order

---
+++ panda_sorting-columns.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+ 
+unsorted_df = pd.DataFrame(np.random.randn(6,4),index=[1,4,2,3,5,0],
+                           columns = ['col2','col1', 'col4', 'col3'])
+
+print("Original DataFrame:\n", unsorted_df)
+
+# sorting the dataframe columns
+sorted_df = unsorted_df.sort_index(axis=1, ascending=False)
+print("\nSorted DataFrame:\n", sorted_df)
