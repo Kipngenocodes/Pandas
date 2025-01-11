4b49eeaa73058a23b68542b6b3dd7c535e0d2029
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-11 10:49:58 -0800

    refilling while reindexing like in pandas

---
+++ refilling_while_reindexing_like.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+'''
+The reindex() method provides an optional parameter method for filling missing values.
+The available methods include −
+    pad/ffill: Fill values forward.
+    bfill/backfill: Fill values backward.
+    nearest: Fill from the nearest index values.
+'''
+
+df1 = pd.DataFrame(np.random.randn(6, 3), columns=['col1', 'col2', 'col3'])
+df2 = pd.DataFrame(np.random.randn(2, 3), columns=['col1', 'col2', 'col3'])
+
+# # Padding NaNs
+print(df2.reindex_like(df1))
+
+# Now fill the NaNs with preceding values
+print("Data Frame with Forward Fill:")
+print(df2.reindex_like(df1, method='ffill'))
