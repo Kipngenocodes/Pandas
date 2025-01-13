b4ef62d93b9619827926c7426caf064df95dd0b2
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-13 11:45:05 -0800

    working eith winows functions

---
+++ windows_functions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+'''
+Rolling Window: A sliding window that can be fixed or variable in size.
+Weighted Window: A non-rectangular, weighted window supplied by the scipy.signal library.
+Expanding Window: An accumulating window that includes all data points up to the current one.
+Exponentially Weighted Window: An accumulating window that 
+applies exponential weighting to previous data points.'''
+
+
+# create a dataframe
