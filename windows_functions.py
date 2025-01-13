c5278ac47441ce98faad66b9623a4ffda0a0f2e5
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-13 14:06:58 -0800

    working with windows functions

--- windows_functions.py
+++ windows_functions.py
@@ -9,4 +9,12 @@ Exponentially Weighted Window: An accumulating window that
 applies exponential weighting to previous data points.'''
 
 
-# create a dataframe
+# Create a dataframe with random integers
+df = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)), columns=['a', 'b', 'c', 'd'])
+
+print('\n Original DataFrame: \n', df)
+
+# showing a  rolling window of 3 rows
+rolling_window = df.rolling(window=3).mean()
+
+print ('\n Rolling Window of 3 rows: \n', rolling_window)
