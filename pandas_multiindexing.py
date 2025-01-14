433ed77e60fc3207cdae62f2fc3817e73992761b
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-14 14:34:28 -0800

    working with multiindexing

---
+++ pandas_multiindexing.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+'''
+ powerful feature in pandas that allows you to work with
+ higher-dimensional data in lower-dimensional structures like Series (1D) and DataFrame (2D). '''
