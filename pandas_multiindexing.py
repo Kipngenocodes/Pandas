4b6fa6bc09225e035980457316ca91b40c39ee9a
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-14 14:43:24 -0800

    working with multiindexing

--- pandas_multiindexing.py
+++ pandas_multiindexing.py
@@ -4,3 +4,18 @@ import numpy as np
 '''
  powerful feature in pandas that allows you to work with
  higher-dimensional data in lower-dimensional structures like Series (1D) and DataFrame (2D). '''
+ 
+#  create a MultiIndex object in pandas, including from lists of arrays, tuples, products of iterables, 
+# or directly from a DataFrame
+'''
+MultiIndex.from_arrays()
+MultiIndex.from_product()
+MultiIndex.from_tuples()
+MultiIndex.from_frame()'''
+
+# Creating MultiIndex from Lists of Arrays
+list_of_arrays = [['Kipngeno', 'Samuel', 'Robert',  'Fancy', 'Gina', 'Monalisa'],
+                  ['a', 'b', 'c', 'd', 'e', 'f']]
+
+# create a multiindex from the list of arrays
+multi_index = pd.MultiIndex.from_arrays(list_of_arrays, names =['Name', 'Letter'])
