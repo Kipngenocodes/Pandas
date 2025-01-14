3b8343f527fddd5841783c59609fce22d226244c
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-13 16:07:48 -0800

    pandas merging like sql

--- pandas_merging_joining.py
+++ pandas_merging_joining.py
@@ -29,3 +29,29 @@ print("\nOriginal Right DataFrame: \n", right_dataframe)
 # merging the two dataframes on 'id' column
 r= pd.merge(left_dataframe, right_dataframe, on='id')
 print("\nMerged DataFrame: \n", r)
+
+# Merge Using 'how' Argument
+'''The how argument to merge specifies how to determine which 
+keys are to be included in the resulting table. If a key combination
+does not appear in either the left or the right tables, 
+the values in the joined table will be NA.
+
+left	LEFT OUTER JOIN	- Use keys from left object
+right	RIGHT OUTER JOIN - Use keys from right object
+outer	FULL OUTER JOIN	 - Use union of keys from both objects
+inner	INNER JOIN	- Use intersection of keys'''
+
+# using 'left' argument to perform left outer join
+left_outer_join = pd.merge(left_dataframe, right_dataframe, how='left', on='id')
+
+# using 'right' argument to perform right outer join
+right_outer_join = pd.merge(left_dataframe, right_dataframe, how='right', on='id')
+# using 'outer' argument to perform full outer join
+full_outer_join = pd.merge(left_dataframe, right_dataframe, how='outer', on='id')
+# using 'inner' argument to perform inner join
+inner_join = pd.merge(left_dataframe, right_dataframe, how='inner', on='id')
+
+print("\nLeft Outer Join: \n", left_outer_join)
+print("\nRight Outer Join: \n", right_outer_join)
+print("\nFull Outer Join: \n", full_outer_join)
+print("\nInner Join: \n", inner_join)
