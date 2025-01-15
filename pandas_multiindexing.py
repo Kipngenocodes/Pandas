8e49f2aa5d5fe9b364314eca301fb73e4ef7cc98
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-14 21:00:30 -0800

    working with multindexing

--- pandas_multiindexing.py
+++ pandas_multiindexing.py
@@ -19,3 +19,31 @@ list_of_arrays = [['Kipngeno', 'Samuel', 'Robert',  'Fancy', 'Gina', 'Monalisa']
 
 # create a multiindex from the list of arrays
 multi_index = pd.MultiIndex.from_arrays(list_of_arrays, names =['Name', 'Letter'])
+
+# create a multiindex series 
+series = pd.Series(np.random.randint(0, 100, 6), index = multi_index)
+
+print('\n the series is \n', series)
+
+print('\n')
+
+# creation of the multiindexing from tuples
+tuple_list = [('Kipngeno', 'a'), ('Samuel', 'b'), ('Robert', 'c'), ('Fancy', 'd'),  ('Sammy', 'K')]
+
+# create a multiindex from the tuple list
+multi_index = pd.MultiIndex.from_tuples(tuple_list, names = ['Name', 'Letter'])
+# create a multiindex dataframe
+df = pd.DataFrame(np.random.randint(0, 100, (5, 2)), index = multi_index, columns = ['Score1', 'Score2'])
+print('\n the dataframe is \n', df)
+
+
+
+# Create a multiindex from the product of iterables
+iterables = [['a', 'b', 'c'], [1, 2, 3], ['Green', 'Red', 'Yellow']]
+multi_index = pd.MultiIndex.from_product(iterables, names=['Letter', 'Number', 'Colors'])
+
+# Create a DataFrame from the multiindex
+df = pd.DataFrame(np.random.randn(27, 3), index=multi_index, columns=["A", "B", "C"])
+
+# Output the DataFrame
+print('\nThe DataFrame is:\n', df)
