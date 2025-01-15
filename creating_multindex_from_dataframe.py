92eb74b6d43583fd1a72fed4a539986056e48083
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-14 21:21:27 -0800

    creating a multiindex from a dataframe

--- creating_multindex_from_dataframe.py
+++ creating_multindex_from_dataframe.py
@@ -2,5 +2,15 @@ import pandas as pd
 import numpy as np
 
 # create a datafame with 5 rows and 3 columns
-df = pd.DataFrame(np.random.randint(0,100,size=(5, 3)), columns=list ('ABC'))
+df = pd.DataFrame(np.random.randint(0,100,size=(4, 3)), columns=list ('ABC'))
 print(df)
+
+print('\n')
+# create a multiindex object 
+multi_index = pd.MultiIndex.from_frame(df[['A', 'B']])
+
+# Create a new MultiIndexed DataFrame
+multi_df = pd.DataFrame(np.random.randn(4, 3), index=multi_index, columns=["A", "B", "C"])
+
+# Display the output MultiIndexed DataFrame
+print("Output MultiIndexed DataFrame:\n", multi_df)
