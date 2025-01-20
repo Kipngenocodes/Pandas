8dd5e9ef532eada8ecf0d6ff035536b84d0fae6f
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-19 23:12:33 -0800

    Bolean Indexing wth multiindexing in pandas

---
+++ Boolean_indexing_with_multiindex.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+# Explanation of MultiIndexed DataFrames
+'''
+Pandas MultiIndexed objects allow you to apply boolean indexing 
+to filter data based on conditions. 
+It creates a mask and applies it to the DataFrame.
+'''
+
+# Creation of MultiIndex object
+index = pd.MultiIndex.from_tuples([
+    ('A', 'x'), ('A', 'y'), 
+    ('B', 'x'), ('B', 'y'),
+    ('C', 'x'), ('C', 'y'),
+    ('D', 'x'), ('D', 'y')
+])
+
+# Create a DataFrame with random data
+df = pd.DataFrame(
+    np.random.randint(0, 100, size=(8, 4)),  # Matching rows with the index
+    index=index,
+    columns=list('ABCD')
+)
+
+# Display the input DataFrame
+print('Original MultiIndexed DataFrame:\n', df)
