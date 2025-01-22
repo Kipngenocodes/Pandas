8542b6db7a652a9751e84ad51de49f8140274795
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-21 22:32:10 -0800

    working with specific index

---
+++ Sorting_MultiIndex_Specific_Level.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Create a MultiIndex object
+index = pd.MultiIndex.from_tuples([('C', 'one'), ('C', 'two'),('B', 'one'), ('B', 'two')])
+
+# Create a DataFrame
+data = [[1, 2], [3, 4], [5, 6], [7, 8]]
+df = pd.DataFrame(data, index=index, columns=['X', 'Y'])
+
+# Display the input DataFrame
+print('Original MultiIndexed DataFrame:\n',df)
+
+# Sort MultiIndex by the first level
+sorted_df = df.sort_index(level=0)
+print("Resultant DataFrame:")
+print(sorted_df)
+
