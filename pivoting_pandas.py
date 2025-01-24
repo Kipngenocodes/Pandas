6360838fac6d68097b99a4e8487d7f37319c4aa7
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-23 23:05:25 -0800

    working  with pivoting

---
+++ pivoting_pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+''''
+Pivoting in Python Pandas is a powerful data transformation
+technique that reshapes data for easier analysis and visualization.
+It change the data representation from a "long" format to a "wide" format,
+making it simpler to perform aggregations and comparisons.'''
+
+# Creating a Dataframe
+df = pd.DataFrame({"Col1": range(12),"Col2": ["A", "A", "A", "B", "B","B", "C", "C", "C", "D", "D", "D"],
+"date": pd.to_datetime(["2024-01-03", "2024-01-04", "2024-01-05"] * 4)})
+
+# output the  following dataframe:
+print('\n Original DataFrame:\n', df)
