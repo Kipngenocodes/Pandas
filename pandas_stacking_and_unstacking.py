31a07893e6afb6f84bb4359ff5d6f1c9456a6295
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-23 23:48:33 -0800

    p

--- pandas_stacking_and_unstacking.py
+++ pandas_stacking_and_unstacking.py
@@ -9,3 +9,18 @@ Whether it's compressing columns into row levels or expanding rows into columns,
 these operations are crucial for handling complex datasets.
 Pandas library provides two main methods for this stacking and 
 unstacking operations which are stack() and unstack().'''
+
+# Create MultiIndex
+tuples = [["x", "x", "y", "y", "", "f", "z", "z"],["1", "2", "1", "2", "1", "2", "1", "2"]]
+
+index = pd.MultiIndex.from_arrays(tuples, names=["first", "second"])
+
+# Create a DataFrame
+df = pd.DataFrame(np.random.randn(8, 2), index=index, columns=["A", "B"])
+
+# Display the input DataFrame
+print('Input DataFrame:\n', df)
+
+# Stack columns
+stacked = df.stack()
+print('\nOutput Reshaped DataFrame:\n', stacked)
