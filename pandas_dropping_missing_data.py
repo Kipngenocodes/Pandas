f8450dde0562b7d65003fceb397bec36eb1be94b
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-28 23:15:25 -0800

    Handling missing data in python pandas

--- pandas_dropping_missing_data.py
+++ pandas_dropping_missing_data.py
@@ -20,3 +20,19 @@ print('\n Original Dataframe: \n', df)
 df_dropped = df.dropna()
 # output the dataframe
 print('\n Dataframe after dropping missing values: \n', df_dropped)
+
+'''
+Syntax:
+DataFrame.dropna(*, axis=0, how=<no_default>, 
+thresh=<no_default>, subset=None, inplace=False, ignore_index=False)
+
+where:
+    axis: 0 or 'index' (default) to drop rows; 1 or 'columns' to drop columns.
+    how: By default it is set to 'any', which drops that row or column if any missing values are present. If set to 'all', then it drops that row or column if all the missing values.
+    thresh: Require a minimum number of non-NA values to retain the row or column.
+    subset: List of specific columns (if dropping rows) or rows (if dropping columns) to consider.
+    inplace: Modify the DataFrame in place (default is False).
+    ignore_indexReset the index of the result (default is False).
+'''
+
+
