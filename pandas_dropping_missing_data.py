98a4d04ccc4325480c7cb0de682062ccf42efcdc
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-28 23:11:28 -0800

    Handling missing data in python pandas

--- pandas_dropping_missing_data.py
+++ pandas_dropping_missing_data.py
@@ -8,11 +8,15 @@ The Python Pandas library provides an easy way for removing rows or columns
 that contain missing values (NaN or NaT) from a dataset using the dropna() method.
 '''
 # Create a dataframe with random data and include missing data 
-df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list ('ABCD'))
-df.loc[10:20, 'A'] = np.nan
-df.loc[30:40, 'B'] = np.nan
-df.loc[50:60, 'C'] = np.nan
-df.loc[70:80, 'D'] = np.nan
+df = pd.DataFrame(np.random.randint(0,20,size=(20, 4)), columns=list ('ABCD'))
+df.loc[0:5, 'A'] = np.nan
+df.loc[6:9, 'B'] = np.nan
+df.loc[16:19, 'D'] = np.nan
 
 # output the dataframe
 print('\n Original Dataframe: \n', df)
+
+# Dropping the missing values from the dataframe
+df_dropped = df.dropna()
+# output the dataframe
+print('\n Dataframe after dropping missing values: \n', df_dropped)
