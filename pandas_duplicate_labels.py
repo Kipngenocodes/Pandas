44a1028b2115d12d9a288fdce7b949aec7665f04
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-30 14:35:03 -0800

    working with pandas duplicate labels

--- pandas_duplicate_labels.py
+++ pandas_duplicate_labels.py
@@ -7,7 +7,7 @@ If a dataset contains the repeated index labels then we call it as duplicated la
 it can lead to unexpected results in some operations such as filtering, aggregating, or slicing.
 '''
 # Create a DataFrame with duplicated index labels
-df = pd.DataFrame({'A': [1, 2, 3], 'B': [ 4, 5, 6]}, index=[1, 1, 2])
+df = pd.DataFrame({'A': [1, 2, 3], 'B': [ 1, 2, 6]}, index=[1, 1, 2])
 print(df)
 
 # Checking for duplicate labels 
@@ -15,3 +15,8 @@ print(df)
 Use the pandas Index.is_unique attribute. 
 If it returns False, then it means there are duplicate labels in your Index.
 '''
+# Check if the row index is unique
+print("Is row index is unique:",df.index.is_unique)  
+
+# Check if the column index is unique
+print('Is column index is unique:',df.columns.is_unique) 
