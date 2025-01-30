79cb827259d289405d18b1610cd56929cdbbf199
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-29 23:05:54 -0800

    Pandas duplicates

--- pandas_duplicate_data.py
+++ pandas_duplicate_data.py
@@ -24,3 +24,20 @@ results = df.duplicated()
 
 # output the results 
 print('\n Showing the duplicates in dataframe\n', results)
+
+# Identifying Duplicates in a DataFrame with specific columns 
+# Find duplicates in the DataFrame
+result_duplicate = df.duplicated(subset=['Name', 'Score'])
+
+# Display the resultant Duplicates
+print('\nResult after finding the duplicates:')
+# print(result_duplicate) 
+
+
+# Drop duplicates in the DataFrame
+
+resultant = df.drop_duplicates()
+
+# Display the resultant Duplicates
+print('\nResult after finding the duplicates:')
+print(resultant) 
