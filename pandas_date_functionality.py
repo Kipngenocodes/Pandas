a2b0a98fd60b57ab26cfc3045a7ab51077defa66
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-31 14:48:30 -0800

    working  with date functionality

--- pandas_date_functionality.py
+++ pandas_date_functionality.py
@@ -5,3 +5,11 @@ import numpy as np
 # Create a date range 
 date_range = pd.date_range('2022-01-01', periods=10, freq=' D')
 print(date_range)
+
+# Changing date frequency 
+date_range = pd.date_range('2022-01-01', periods=10, freq='ME')
+print(date_range)
+
+# bdate_range() stands for business date ranges. Unlike date_range(), it excludes Saturday and Sunday.
+date_range = pd.bdate_range('2022-01-01', periods=10)
+print(date_range)
