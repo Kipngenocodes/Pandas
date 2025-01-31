b62f8235c1b38f3a88343a0da12cec2a63b02598
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-01-31 14:28:27 -0800

    working with groupby

--- pandas_groupby.py
+++ pandas_groupby.py
@@ -10,3 +10,17 @@ df = pd.DataFrame(ipl_data)
 
 print(df)
 
+# Splitting data into groups 
+df_grouped = df.groupby(['Team','Year'])
+print(df_grouped)
+
+# Pandas object can be split into any of their objects. There are multiple ways to split an object like −
+
+'''
+obj.groupby('key')
+obj.groupby(['key1','key2'])
+obj.groupby(key,axis=1)
+'''
+# View group
+print (df.groupby('Team').groups)
+
